0e3eb90cc1f5455bc4740efb235ff5a5934d3792
Alexander Smolin <asmolin1993@gmail.com>
2018-06-03 03:10:48 +0300

    Настройки БД для локальной разработки

--- application/models/sql.php
+++ application/models/sql.php
@@ -1,4 +1,4 @@
 <?php 
-	/*$link = mysqli_connect('adventure.my','root','','adventure');*/
-	$link = mysqli_connect("a228420.mysql.mchost.ru", "a228420_1", "goR873nb", "a228420_1");
+	$link = mysqli_connect('adventure.my','root','','adventure');
+	/*$link = mysqli_connect("a228420.mysql.mchost.ru", "a228420_1", "goR873nb", "a228420_1");*/
  ?>
